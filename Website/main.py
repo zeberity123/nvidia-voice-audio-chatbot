e3b708f7ddc75874807e4eed8c6ba22bc7972a19
Baek Dong Ryul <dbstickman@gmail.com>
2024-04-23 11:47:17 +0900

    Updating NodeJS for Tailwindcss application

--- Website/main.py
+++ Website/main.py
@@ -6,14 +6,14 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 # directory of html templates is a folder for jinja2 templates
-templates = Jinja2Templates(directory="templates")
+dist = Jinja2Templates(directory="dist")
 
 VOCALS = [{"name": "Voice Separation", "type": "mp3"}, {"name": "Additional Info", "type": "Text"}, {"name": "Singer Info", "type": "meta data"}]
 
 # create api inpoint for jinja2 templates
 @app.get('/')
 async def name(request: Request):
-    return templates.TemplateResponse("mp3_upload.html", {"request": request})
+    return dist.TemplateResponse("index.html", {"request": request})
     # return templates.TemplateResponse("home.html", {"request": request, "name": "NVauC", "vocals": VOCALS})
 #cd Website
 # uvicorn main:app --reload
