d97679000ac6bcfa5a4e294dd84450cd862e0c04
Baek Dong Ryul <dbstickman@gmail.com>
2024-04-23 16:45:15 +0900

    The end of this commit. This method is too complicated

--- Website/main.py
+++ Website/main.py
@@ -7,14 +7,15 @@ app = FastAPI()
 
 # directory of html templates is a folder for jinja2 templates
 dist = Jinja2Templates(directory="dist")
+templates = Jinja2Templates(directory="templates")
 
 VOCALS = [{"name": "Voice Separation", "type": "mp3"}, {"name": "Additional Info", "type": "Text"}, {"name": "Singer Info", "type": "meta data"}]
 
 # create api inpoint for jinja2 templates
 @app.get('/')
 async def name(request: Request):
-    return dist.TemplateResponse("index.html", {"request": request})
-    # return templates.TemplateResponse("home.html", {"request": request, "name": "NVauC", "vocals": VOCALS})
+    # return dist.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("mp3_upload.html", {"request": request})
 #cd Website
 # uvicorn main:app --reload
 
