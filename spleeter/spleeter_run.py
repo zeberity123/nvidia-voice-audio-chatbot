ba2fefd048e5f2549e52ddee3eebd0264cfcefd1
zeberity123 <darudayu123@gmail.com>
2024-04-25 11:11:21 +0900

    modified spleeter_run.py to separate multiple audio files in custom directory

--- spleeter/spleeter_run.py
+++ spleeter/spleeter_run.py
@@ -1,31 +1,36 @@
 import subprocess
-import os
-
-# python -m spleeter train -p configs/vocadb_config.json -d C:/musdb18hq --verbose
-
-# spleeter separate -o results/melan_90k -p spleeter:4stems test_audio/melancholic_mixture.mp3
 
 # music separation
-model_steps = '130' # [50, 75, 100, 130]
+model_steps = '210' # [50, 75, 100, 130, 210]
 model_data = 'vocadb'
 song_names = [
-    'melancholic_mixture.mp3',
-    'raspberry_monster_mixture.wav',
-    'melt_mixture.wav',
-    'amanojaku_mixture.wav',
+    'itte_mixture.mp3'
+    # 'melancholic_mixture.mp3',
+    # 'raspberry_monster_mixture.wav',
+    # 'melt_mixture.wav',
+    # 'amanojaku_mixture.wav',
     # 'black_rock_shooter_mixture.wav',
     # 'loki_mixture.wav',
     # 'tengaku_mixture.wav',
     # 'world_is_mine_mixture.wav'
 ]
 
+instruments = ['bass', 'drums', 'other', 'vocals']
+
 for song_name in song_names:
-    pre_folder = song_name.split('mixture.')[0]
-    out_loc = f'results/vocadb_{model_steps}k/{pre_folder}{model_steps}k_'
+    name_split = song_name.split('.')[0]
+    short_name = name_split.split('_')[0]
+    pre_folder = name_split[:-7]
+    out_loc = f'results/vocadb_{model_steps}k/{pre_folder}{model_steps}k'
     in_loc = f'test_audio/{song_name}'
-    command = ['spleeter', 'separate', '-o', out_loc, '-p', 'spleeter:4stems', in_loc]
-    C:\Users\hancom05\Desktop\nvidia-voice-audio-chatbot\spleeter\results\vocadb_130k\melancholic_130k_\melancholic_mixture
-    command = ['mv', ]
 
+    commands = [
+        ['spleeter', 'separate', '-o', out_loc, '-p', 'spleeter:4stems', in_loc]
+    ]
 
-    subprocess.call(command)
+    for i in instruments:
+        commands.append(['mv', f'{out_loc}/{name_split}/{i}.*', f'{out_loc}/{short_name}_{model_steps}k_{i}.wav'])
+    commands.append(['rm', '-r', f'{out_loc}/{name_split}'])
+
+    for command in commands:
+        subprocess.call(command)
