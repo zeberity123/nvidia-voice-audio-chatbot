6d8c2e2ad57c411424c6cd1c7897921be364827b
zeberity123 <darudayu123@gmail.com>
2024-04-29 12:46:08 +0900

    added delete_files

--- chatbot-ish/run_spleeter.py
+++ chatbot-ish/run_spleeter.py
@@ -1,35 +1,40 @@
 import subprocess
 import os
 
+INSTRUMENTS = ["bass", "drums", "other", "vocals"]
+OUT_LOC = 'processed_files'
+IN_LOC = 'uploaded_files'
 
 def run_spleeter(filename):
     song_names = []
-
-    instruments = ["bass", "drums", "other", "vocals"]
-
     song_names.append(filename)
 
     for song_name in song_names:
         name_split = song_name.split(".")[0]
         pre_folder = name_split[:]
 
-        out_loc = f"processed_files/"
-        in_loc = f"uploaded_files/{song_name}"
+        in_loc = f"{IN_LOC}{song_name}"
 
         commands = [
-            ["spleeter", "separate", "-o", out_loc,
+            ["spleeter", "separate", "-o", OUT_LOC,
                 "-p", "spleeter:4stems", in_loc]
         ]
 
-        for i in instruments:
+        for i in INSTRUMENTS:
             commands.append(
                 [
                     "mv",
-                    f"{out_loc}/{name_split}/{i}.*",
-                    f"{out_loc}/{pre_folder}_{i}.wav",
+                    f"{OUT_LOC}/{name_split}/{i}.*",
+                    f"{OUT_LOC}/{pre_folder}_{i}.wav",
                 ]
             )
-        commands.append(["rm", "-r", f"{out_loc}/{name_split}"])
+        commands.append(["rm", "-r", f"{OUT_LOC}/{name_split}"])
 
         for command in commands:
             subprocess.call(command)
+
+
+def delete_files(filename):
+    name_split = filename.split(".")[0]
+    command = ['rm', f"{OUT_LOC}/{name_split}*.wav"]
+    subprocess.call(command)
